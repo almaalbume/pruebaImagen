15ff614bda1222d5fd448b4af857a9de64d335b1
Mariano Búrbano <almaalbume@hotmail.com>
2018-02-07 09:10:13 -0600

    ADD: First COmmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { File } from '@ionic-native/file';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    File,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
+import { File } from '@ionic-native/file';
+import * as $ from "jquery";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private file: File) {
 
+  }
+
+  generatePdf() {
+    //console.log(document.querySelector("#capture"))
+
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+      //   console.log(canvas)
+
+      var doc = new jsPDF("p", "mm", "a4");
+      let imgData = canvas.toDataURL("image/PNG");
+      console.log(imgData)
+      //doc.addImage(imgData, 'PNG', 20, 20);
+      
+      //let pdfOutput = doc.output();
+      
+      //   let buffer = new ArrayBuffer(pdfOutput.length);
+      //   let array = new Uint8Array(buffer);
+
+      //   for (var i = 0; i < pdfOutput.length; i++) {
+      //     array[i] = pdfOutput.charCodeAt(i);
+      //   }
+
+      // const directory = this.file.externalApplicationStorageDirectory;
+
+
+      // const fileName = "example.pdf";
+
+
+      // this.file.writeFile(directory, fileName, buffer)
+      //   .then((success) => console.log("File created Succesfully" + JSON.stringify(success)))
+      //   .catch((error) => console.log("Cannot Create File " + JSON.stringify(error)));
+
+
+
+
+    });
   }
 
 }
